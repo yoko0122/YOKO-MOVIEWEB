cca33f11554c3bfd1f77791113b082b9377fb26f
25LP5306 25LP5306 <25LP5306@pinecones-iMac.local>
2025-05-27 09:23:05 +0800

    djvkl

--- src/component/MovieCard.jsx
+++ src/component/MovieCard.jsx
@@ -1,4 +1,5 @@
 import { Star } from "lucide-react";
+import Link from "next/link";
 
 export const MovieCard = ({ upComing, Popular, topRated }) => {
   const upComes = `${process.env.NEXT_PUBLIC_TMDB_IMAGE_SERVICE_URL}/original${
@@ -8,7 +9,9 @@ export const MovieCard = ({ upComing, Popular, topRated }) => {
   return (
     <div>
       <div className=" bg-secondary border-1 rounded-2xl flex flex-col justify-center w-full ">
-        <img src={upComes} className="  md:h-[pull] rounded-t-2xl" />
+        <Link href={`/details/${(upComing?.id, Popular?.id, topRated?.id)}`}>
+          <img src={upComes} className="  md:h-[pull] rounded-t-2xl" />
+        </Link>
         <div>
           <div className="flex flex-col gap-2 md:gap-4 md:pl-4 ">
             <div className="flex gap-1 pt-2 ">
